16e72d3094cd273a136f434194f2d2aadc96dca5
Sibi <sibisimon1@gmail.com>
2019-05-26 00:48:52 +0100

    Initial commit

---
+++ ann_breast_cancer.py
@@ -0,0 +1,394 @@
+"""
+Author: Sibi Simon - 18201381
+Date: 25-11-2018
+"""
+
+# Importing modules
+
+import numpy as np
+import pandas as pd
+from sympy import *
+from scipy import stats
+
+# Importing modules for pytorch
+import torch
+from torch.autograd import Variable
+import torch.utils.data as data_utils
+import torch.nn.init as init
+
+# Importing modules for keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils import plot_model
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+
+# Visualization import
+from plotly.offline import plot
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+
+PATH = "pytorch_breast_cancer.csv"
+
+
+def main_process():
+    # Reading dataset
+    data_set = pd.read_csv(PATH)
+
+    # Data Analysis using summary, bar & confusion
+    print('------------------------------------------ VISUALIZATION --------------------------------------------')
+    plts = Visualize(df=data_set)
+    plts.summary()
+    plts.bar()
+
+    # Data pre-processing (wrangling)
+    print('----------------------------------------- PRE-PROCESSING --------------------------------------------')
+    pre_process = PreProcessing(df=data_set)
+    pre_process.remove_outliers()
+    pre_process.modify_col()
+    pre_process.test_train_split()
+    data_loader, test_data, test_validation, y_test_pyt = pre_process.scaling()
+
+    # Modelling using pytorch
+    print('--------------------------------------- PYTORCH MODELLING ------------------------------------------')
+    pyt_sq_model = PyTorchModel(data_loader=data_loader, test_data=test_data, test_validation=test_validation)
+    pyt_sq_model.create()
+    pyt_sq_model.run()
+    predictd_pyt = pyt_sq_model.predict()
+    history_pyt = pyt_sq_model.get_history
+
+    # Modelling using keras
+    print('--------------------------------------- KERAS MODELLING --------------------------------------------')
+    x_train, x_test, y_train, y_test = pre_process.krs_test_train_split()
+    krs_sq_model = KerasModel(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    krs_sq_model.create()
+    krs_sq_model.compile()
+    history_krs=krs_sq_model.fit()
+    krs_sq_model.evaluate()
+    predictd_krs = krs_sq_model.predict()
+
+    # Diagnosing Model
+    print('-------------------------------- DIAGNOSING AND COMPARING MODELS --------------------------------')
+
+    # Plots for pytorch
+    plts.history_pytorch(history_pyt, type_plt='loss', title="Model Loss plot(Pytorch) in Percentage")
+    plts.history_pytorch(history_pyt, type_plt='accuracy', title="Pytorch Model Accuracy plot")
+    plts.confusion_matrix(predictd_pyt, y_test_pyt, title="Confusion matrix(Pytorch)")
+
+    # Plots for keras
+    plts.history_keras(history_krs, type_plt='loss', title="Keras Model Loss plot")
+    plts.history_keras(history_krs, type_plt='acc', title="Keras Model Loss plot")
+    plts.confusion_matrix(predictd_krs, y_test, title="Confusion matrix(Keras)")
+
+
+class Visualize(object):
+    """
+    class for visualizingfor
+    """
+    def __init__(self, *args, **kwargs):
+        self.df = kwargs['df']
+
+    def summary(self):
+        print(self.df.describe())
+
+    def bar(self):
+        output_col = self.df['diagnosis'].tolist()
+        data = [go.Bar(
+            x=['Malignant', 'Benign'],
+            y=[output_col.count('M'), output_col.count('B')],
+            text=[output_col.count('M'), output_col.count('B')],
+            textposition='auto',
+            marker=dict(
+                color='rgb(158,202,225)',
+                line=dict(
+                    color='rgb(8,48,107)',
+                    width=1.5),
+            ),
+            opacity=0.8
+
+        )]
+        layout = go.Layout(
+            title='Frequency of Malignant and Benign',
+            width=600,
+            height=600
+        )
+
+        fig = go.Figure(data=data, layout=layout)
+
+        plot(fig, filename='cancer_type.html')
+
+    def confusion_matrix(self, y_pred, y_actual, title=''):
+
+        labels = ['Malignant', 'Benign']
+        c_matrix = confusion_matrix(y_actual, y_pred)
+        fig = ff.create_annotated_heatmap(c_matrix, x=labels, y=labels)
+        fig.layout.title = title
+        fig.layout.width = 400
+        fig.layout.height = 400
+        plot(fig, filename='confusion_matrix.html')
+
+
+    def history_pytorch(self, history, type_plt='loss', title=''):
+
+        # Accuracy plot and Loss plot
+        trace0 = go.Scatter(
+            x=list(range(0, history['epochs'])),
+            y=history[type_plt],
+            mode='lines',
+            name='Train'
+        )
+        trace1 = go.Scatter(
+            x=list(range(0, history['epochs'])),
+            y=history[type_plt + '_val'],
+            mode='lines+markers',
+            name='Test'
+        )
+        data = [trace0, trace1]
+        layout = go.Layout(
+            title=title,
+            width=600,
+            height=600,
+            xaxis=dict(
+                title='Epochs',
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            ),
+            yaxis=dict(
+                title=type_plt,
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            )
+        )
+        fig = go.Figure(data=data, layout=layout)
+        plot(fig, filename='history_py.html')
+
+
+    def history_keras(self, history, type_plt='loss', title=''):
+
+        # Accuracy plot and Loss plot
+        trace0 = go.Scatter(
+            x=history.epoch,
+            y=history.history[type_plt],
+            mode='lines',
+            name='Train'
+        )
+        trace1 = go.Scatter(
+            x=history.epoch,
+            y=history.history['val_' + type_plt],
+            mode='lines+markers',
+            name='Test'
+        )
+        data = [trace0, trace1]
+        layout = go.Layout(
+            title=title,
+            width=600,
+            height=600,
+            xaxis=dict(
+                title='Epochs',
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            ),
+            yaxis=dict(
+                title=type_plt,
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            )
+        )
+        fig = go.Figure(data=data, layout=layout)
+        plot(fig, filename='history_kr.html')
+
+
+class PreProcessing(object):
+    """
+    Class for performing data pre-processing operations such as data cleansing, wrangling etc.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.df = kwargs['df']
+        self.y_out = []
+        self.x_in = []
+        self.x_train = []
+        self.y_train = []
+        self.x_test = []
+        self.y_test = []
+
+    def modify_col(self):
+        bin_output = {"M": 1, "B": 0}  # modifying output data to binary(0, 1)
+        self.y_out = self.df["diagnosis"].replace(bin_output)
+        self.x_in = self.df.drop(["id", "diagnosis", "Unnamed: 32"], axis=1)
+
+    def remove_outliers(self):
+        # Avoiding outlier check for these columns because they are not continuous or normally distributed
+        total_data_points = len(self.df)
+        df_temp = self.df.drop(["id", "diagnosis", "Unnamed: 32"], axis=1)
+        self.df = pd.DataFrame(self.df[(np.abs(stats.zscore(df_temp)) <= 4).all(axis=1)])
+        print("Number of Outliers:", total_data_points - len(self.df))
+        print("Number of rows without outliers:", len(self.df))
+
+    def test_train_split(self):
+       self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x_in, self.y_out, test_size=0.2,
+                                                                               random_state=85)
+
+    def scaling(self):
+
+        # Scaling train data
+        scaling_train = StandardScaler()
+        transformed = scaling_train.fit_transform(self.x_train)
+        train_data = data_utils.TensorDataset(torch.from_numpy(transformed).float(), torch.from_numpy(
+            self.y_train.as_matrix()).float())
+        data_loader = data_utils.DataLoader(train_data, batch_size=128, shuffle=False)
+
+        # Scaling test data
+        scaling_test = StandardScaler()
+        transformed = scaling_test.fit_transform(self.x_test)
+        test_data = torch.from_numpy(transformed).float()
+        test_validation = torch.from_numpy(self.y_test.as_matrix()).float()
+
+        return data_loader, test_data, test_validation, self.y_test
+
+    def test_train_split(self):
+        scaling_test = StandardScaler()
+        transformed = scaling_test.fit_transform(self.x_in)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(transformed, self.y_out, test_size=0.3,
+                                                                                random_state=85)
+
+    def krs_test_train_split(self):
+        scaling_train = StandardScaler()
+        x_in = scaling_train.fit_transform(self.x_in)
+        return train_test_split(x_in, self.y_out, test_size=0.3, random_state=85)
+
+
+class PyTorchModel(object):
+    """
+    Pytorch model building, diagnosing
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.train_data_loader = kwargs['data_loader']
+        self.test_data = kwargs['test_data']
+        self.test_validation = kwargs['test_validation']
+        self.model = ''
+        self.learning_rate = 0.0006
+        self.epochs = 350
+        self.history = {"epochs": self.epochs, "loss": [], "accuracy": [], "loss_val": [], "accuracy_val": []}
+
+    def create(self):
+        self.model = torch.nn.Sequential()
+
+        module = torch.nn.Linear(30, 20)
+        init.xavier_normal(module.weight)
+        self.model.add_module("linear - 1", module)
+
+        module = torch.nn.Linear(20, 10)
+        init.xavier_normal(module.weight)
+        self.model.add_module("linear - 2", module)
+
+        module = torch.nn.Linear(10, 1)
+        init.xavier_normal(module.weight)
+        self.model.add_module("linear - 3", module)
+
+        self.model.add_module("rel", torch.nn.ReLU())
+        self.model.add_module("sig", torch.nn.Sigmoid())
+
+    def run(self):
+        loss_func = torch.nn.MSELoss(size_average=False)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+        for epoch in range(self.epochs):
+            loss = None
+
+            for idx, (batch, target) in enumerate(self.train_data_loader):
+                y_pred = self.model(Variable(batch))
+                loss = loss_func(y_pred, Variable(target.float()).reshape(len(Variable(target.float())), 1))
+                prediction = self._n_correct(y_pred)
+                corrected_n = (prediction == target.numpy()).sum()
+
+                y_val_pred = self.model(Variable(self.test_data))
+                loss_val = loss_func(y_val_pred, Variable(self.test_validation.float()).reshape(len(Variable(
+                    self.test_validation.float())), 1))
+                prediction_val = self._n_correct(y_val_pred)
+                corrected_val_n = (prediction_val == self.test_validation.numpy()).sum()
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            self.history["loss"].append(loss.data[0])
+            self.history["accuracy"].append(100 * corrected_n / len(prediction))
+            self.history["loss_val"].append(loss_val.data[0])
+            self.history["accuracy_val"].append(100 * corrected_val_n / len(prediction_val))
+
+            print("Loss, accuracy, val loss, val acc at epoch", epoch + 1, self.history["loss"][-1],
+                  self.history["accuracy"][-1], self.history["loss_val"], self.history["accuracy_val"][-1])
+
+    def _n_correct(self, out):
+        return [1 if x > 0.5 else 0 for x in out.data.numpy()]
+
+    def predict(self):
+        y_pred = self.model(Variable(self.test_data))
+        prediction_val = self._n_correct(y_pred)
+
+        return prediction_val
+
+    @property
+    def get_history(self):
+        return self.history
+
+
+class KerasModel(object):
+    """
+    class for making sequential deep learning model using keras
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.model = Sequential()
+        self.x_train = kwargs['x_train']
+        self.x_test = kwargs['x_test']
+        self.y_train = kwargs['y_train']
+        self.y_test = kwargs['y_test']
+
+    def create(self):
+        self.model.add(Dense(units=16, kernel_initializer='uniform', activation='relu', input_dim=30))
+        self.model.add(Dense(units=8, kernel_initializer='uniform', activation='relu'))
+        self.model.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+        self.model.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+    def summary(self):
+        print(self.model.summary())
+        plot_model(self.model, to_file='model.png')
+
+    def compile(self):
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    def fit(self):
+        history = self.model.fit(self.x_train, self.y_train, batch_size=50, epochs=400, verbose=1,
+                                 validation_data=(self.x_test, self.y_test))
+        return history
+
+    def evaluate(self):
+        score = self.model.evaluate(self.x_test, self.y_test, verbose=1)
+        print("\n")
+        print("Test loss:", score[0])
+
+    def predict(self):
+        y_pred = self.model.predict(self.x_test)
+        prediction_val_krs = self._n_correct(y_pred)
+
+        return prediction_val_krs
+
+    def _n_correct(self, out):
+        return [1 if x > 0.5 else 0 for x in out]
+
+
+if __name__ == "__main__":
+    main_process()
---
+++ ann_house_price.py
@@ -0,0 +1,230 @@
+"""
+Author: Sibi Simon - 18201381
+Date: 25-11-2018
+"""
+
+# Importing modules
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from scipy import stats
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization
+from keras.optimizers import Adam
+from keras.utils import plot_model
+
+from bokeh.plotting import output_notebook, show, figure, gmap
+from bokeh.models import ColumnDataSource, GMapOptions
+import holoviews as hv
+hv.extension('bokeh')
+
+PATH = "keras_kc_house_data.csv"
+
+
+def main_process():
+
+    # Reading dataset
+    price_data_set = pd.read_csv(PATH)
+
+    # Data Analysis using summary, histogram & scatter plots
+    print('----------------------------------- DATA ANALYSIS -----------------------------------------')
+    analysis = DataAnalysis(df=price_data_set)
+    analysis.summary()
+    analysis.housing_location_by_pos()
+    analysis.histogram()
+
+    # Data pre-processing (wrangling)
+    print('----------------------------------- DATA WRANGLING -----------------------------------------')
+    pre_process = PreProcessing(df=price_data_set)
+    pre_process.checking_null_values()
+    pre_process.remove_unwanted_columns()
+    pre_process.remove_outliers()
+    pre_process.scaling()
+    x_train, x_test, y_train, y_test = pre_process.test_train_split()
+
+    # Modelling
+    print('----------------------------------- DATA MODELLING -----------------------------------------')
+    sq_model = SequentialModel(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    sq_model.create()
+    sq_model.compile()
+    history = sq_model.fit()
+    sq_model.evaluate()
+    y_pred = sq_model.predict()
+
+    # Plotting model running history
+    print('----------------------------------- MODEL DIAGNOSIS -----------------------------------------')
+    analysis.plot_history(history)
+    analysis.predict_plot(y_pred, y_test)
+
+
+class PreProcessing(object):
+    """
+    Class for performing data pre-processing operations such as data cleansing, wrangling etc.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.df = kwargs['df']
+
+    def checking_null_values(self):
+        if not self.df.isnull().any().any():
+            print("CHECKING NULL VALUES: No null vlaues in dataset")
+
+    def remove_unwanted_columns(self):
+        self.df = pd.DataFrame(self.df.drop(["date", "id", "zipcode"], axis=1))
+
+    def remove_outliers(self):
+        # Avoiding outlier check for these columns because they are not continuous or normally distributed
+        total_data_points = len(self.df)
+        df_temp = self.df.drop(["yr_renovated", "sqft_basement", "waterfront", "view"], axis=1)
+        self.df = pd.DataFrame(self.df[(np.abs(stats.zscore(df_temp)) <= 4).all(axis=1)])
+        print("Number of Outliers:", total_data_points - len(self.df))
+        print("Number of rows without outliers:", len(self.df))
+
+    def scaling(self):
+        col_train = list(self.df.columns)
+        mat_train = np.matrix(self.df)
+        mat_new = np.matrix(self.df.drop('price', axis=1))
+        mat_y = np.array(self.df.price).reshape((len(self.df), 1))
+
+        pre_processing_y = MinMaxScaler()
+        pre_processing_y.fit(mat_y)
+
+        pre_processing_df = MinMaxScaler()
+        pre_processing_df.fit(mat_train)
+
+        pre_processing_test = MinMaxScaler()
+        pre_processing_test.fit(mat_new)
+
+        self.df = pd.DataFrame(pre_processing_df.transform(mat_train), columns=col_train)
+        print(self.df.head(5))
+
+    def test_train_split(self):
+        columns = list(self.df.columns)
+        features = list(self.df.columns)
+        features.remove('price')
+        training_set = self.df[columns]
+        prediction_set = self.df.price
+        x_train, x_test, y_train, y_test = train_test_split(training_set[features], prediction_set, test_size=0.33,
+                                                            random_state=42)
+        return x_train, x_test, y_train, y_test
+
+
+class DataAnalysis(object):
+    """
+    Class for performing various data analysis and interpretations on data using bokeh plotting
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.df = kwargs['df']
+        self.key = "AIzaSyAM1OHVm6Yr_i54Kt01mylfxyNxQdxmxHQ"
+
+    def summary(self):
+        print(self.df.describe())
+
+    def histogram(self):
+        """
+        Plotting histogram of the response variable
+        """
+        hist, edges = np.histogram(self.df['price'].tolist(), density=False, bins=100)
+        p = figure(title="Response variable Histogram", tools='', background_fill_color="#fafafa")
+        p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+               fill_color="navy", line_color="white")
+        p.xaxis.axis_label = 'Price'
+        p.yaxis.axis_label = 'Frequency'
+        show(p)
+
+    def housing_location_by_pos(self):
+        # Top 25 house with highest price
+        high_price_pos = self.df.sort_values(by='price', ascending=0).head(25)[['lat', 'long']]
+        self._geo_plot(high_price_pos['lat'].tolist(), high_price_pos['long'].tolist(), 'blue', 47.65, -122.25,
+                       "King County, Washington - Highest Price")
+
+        # Top 25 with lowest price
+        lowest_price_pos = self.df.sort_values(by='price', ascending=1).head(25)[['lat', 'long']]
+        self._geo_plot(lowest_price_pos['lat'].tolist(), lowest_price_pos['long'].tolist(), 'red', 47.42, -122.25,
+                       "King County, Washington - Lowest Price")
+
+    def plot_history(self, history):
+        p = figure(plot_width=400, plot_height=400, title="Epoch vs Loss (Test and Train)")
+        p.line(history.epoch, history.history['loss'], line_color="blue", line_width=2, legend='Train')
+        p.line(history.epoch, history.history['val_loss'], line_width=2, line_color='orange', legend='Test')
+        p.xaxis.axis_label = 'Loss'
+        p.yaxis.axis_label = 'Epoch'
+        p.legend.location = "top_right"
+        p.legend.click_policy = "hide"
+        show(p)
+
+    def predict_plot(self, y_pred, y_test):
+        abs_error = [abs(n) for n in list(y_pred.reshape(len(y_pred))) - y_test]
+        p = figure(plot_width=500, plot_height=500, title="Absolute error for test data")
+        p.line(range(0, len(y_pred)), abs_error, line_color="blue", line_width=2)
+        p.xaxis.axis_label = 'Data points'
+        p.yaxis.axis_label = 'Error'
+        p.legend.location = "top_right"
+        p.legend.click_policy = "hide"
+        show(p)
+
+    def _geo_plot(self, lat, long, col, ini_lat, ini_long, title):
+        map_options = GMapOptions(lat=ini_lat, lng=ini_long, map_type="hybrid", zoom=11)
+        p = gmap(self.key, map_options, title=title)
+        source = ColumnDataSource(
+            data=dict(lat=lat,
+                      lon=long)
+        )
+        p.circle(x="lon", y="lat", size=15, fill_color=col, fill_alpha=0.8, source=source)
+        show(p)
+
+
+class SequentialModel(object):
+    """
+    class for making sequential deep learning model using keras
+    """
+    def __init__(self, *args, **kwargs):
+        self.model = Sequential()
+        self.x_train = kwargs['x_train']
+        self.x_test = kwargs['x_test']
+        self.y_train = kwargs['y_train']
+        self.y_test = kwargs['y_test']
+
+    def create(self):
+        self.model.add(Dense(32, activation='relu', input_shape=(17,)))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+
+        self.model.add(Dense(32, activation='relu', input_shape=(50,)))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+
+        self.model.add(Dense(32, activation='relu', input_shape=(50,)))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+
+        self.model.add(Dense(1))
+
+    def summary(self):
+        print(self.model.summary())
+        plot_model(self.model, to_file='model.png')
+
+    def compile(self):
+        self.model.compile(loss='mean_squared_error', optimizer=Adam(), metrics=['mse'])
+
+    def fit(self):
+        history = self.model.fit(self.x_train, self.y_train, batch_size=100, epochs=150, verbose=1,
+                                 validation_data=(self.x_test, self.y_test))
+        return history
+
+    def evaluate(self):
+        score = self.model.evaluate(self.x_test, self.y_test, verbose=1)
+        print("\n")
+        print("Test loss:", score[0])
+
+    def predict(self):
+        y_pred = self.model.predict(self.x_test)
+        return y_pred
+
+
+if __name__ == "__main__":
+    main_process()
